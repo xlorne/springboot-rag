53dae667c60c7625284e51c792f96992d9638d46
lorne <1991wangliang@gmail.com>
2025-03-13 16:02:27 +0800

    add readme

--- src/main/java/com/codingapi/rag/etl/DocumentService.java
+++ src/main/java/com/codingapi/rag/etl/DocumentService.java
@@ -6,8 +6,11 @@ import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Objects;
 
 @Component
 @AllArgsConstructor
@@ -17,11 +20,19 @@ public class DocumentService {
     private final TokenTextSplitter splitter = new TokenTextSplitter();
     private final VectorStore vectorStore;
 
+
+    private Document reBuild(Document document) {
+        String text = Objects.requireNonNull(document.getText());
+        String id = DigestUtils.md5DigestAsHex(text.getBytes(StandardCharsets.UTF_8));
+        return new Document(id, text, document.getMetadata());
+    }
+
     public void importDocuments() {
+        System.out.println("<-----------------importDocuments--------------------->");
         List<Document> documents = localJsonReader.loadDocument();
         documents = splitter.apply(documents);
-
-        vectorStore.add(documents);
+        List<Document> data =  documents.stream().map(this::reBuild).toList();
+        vectorStore.add(data);
 
         String testQuestion = "Trek";
         SearchRequest request = SearchRequest.builder()
@@ -32,7 +43,11 @@ public class DocumentService {
 
 
         System.out.println("Test Question: " + testQuestion);
+        assert searchDocuments != null;
+        System.out.println("searchDocuments size: " + searchDocuments.size());
         System.out.println("searchDocuments: " + searchDocuments);
+
+        System.out.println(">-----------------importDocuments---------------------<");
     }
 
 }
