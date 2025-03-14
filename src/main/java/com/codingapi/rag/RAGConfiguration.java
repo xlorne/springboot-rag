cec2b76ae87b3d7f3085a52862c38ca5bcba44d8
lorne <1991wangliang@gmail.com>
2025-03-14 16:16:48 +0800

    add log

--- src/main/java/com/codingapi/rag/RAGConfiguration.java
+++ src/main/java/com/codingapi/rag/RAGConfiguration.java
@@ -1,9 +1,13 @@
 package com.codingapi.rag;
 
+import com.codingapi.rag.interceptor.LoggingInterceptor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RAGConfiguration {
@@ -11,6 +15,13 @@ public class RAGConfiguration {
     @Bean
     public ChatMemory chatMemory() {
         return new InMemoryChatMemory();
+    }
+
+    @Bean
+    public RestClient.Builder restClient() {
+        return RestClient.builder()
+                .requestFactory(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+                .requestInterceptor(new LoggingInterceptor(true));
     }
 
 }
